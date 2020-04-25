2957c620ad9cfe8dc564b1685d21b9d91b93a833
socratone <gim2origin@gmail.com>
2020-04-25 22:37:00 +0900

    implement basic INTERACTION CLIENT to SERVER

--- index.js
+++ index.js
@@ -2,13 +2,41 @@ const insertButton = document.getElementById('insert');
 const removeButton = document.getElementById('remove');
 const ul = document.getElementById('ul');
 
-insertButton.addEventListener('click', () => {
-  const li = document.createElement('li');
-  const text = document.getElementById('text');
-  li.innerText = text.value;
-  ul.append(li);
-});
+// init
+fetch('http://localhost:3000/api')
+  .then(response => response.json())
+  .then(json => {
+    console.log(json);
+    json.forEach(({ text }) => {
+      const li = document.createElement('li');
+      li.innerText = text;
+      ul.append(li);
+    });
+  })
+  .catch(error => console.log(error));
 
-removeButton.addEventListener('click', () => {
+// insert button
+insertButton.addEventListener('click', () => {
+  const text = document.getElementById('text');
+
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: JSON.stringify({ "text": text.value }),
+    redirect: 'follow'
+  };
+
+  fetch("http://localhost:3000/api", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      console.log('result :', result);
+      const li = document.createElement('li');
+      li.innerText = result.text;
+      ul.append(li);
+    })
+    .catch(error => console.log('error', error));
 
 });
--- store.js
+++
@@ -1,6 +0,0 @@
-const { createStore } = require('redux');
-const reducer = require('./reducer');
-
-const store = createStore(reducer);
-
-module.exports = store;
